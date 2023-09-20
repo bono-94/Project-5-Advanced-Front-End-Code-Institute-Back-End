ad3133c8d3f5d80afb3f927c45a443bc9e1db106
bono-94 <bono.94@hotmail.com>
2023-09-20 10:48:48 +0000

    Register all apps inside their belonging admin.py file

--- posts/admin.py
+++ posts/admin.py
@@ -1,54 +1,9 @@
-# # POST ADMIN
-# @admin.register(Post)
-# class PostAdmin(SummernoteModelAdmin):
+"""
+Admin configuration for managing models in the Django admin site.
+This file registers relevant model.
+"""
 
-#     list_display = (
-#         'post_type',
-#         'title',
-#         'industry',
-#         'author',
-#         'created_on',
-#         'status',
-#         'post_verification',
-#     )
+from django.contrib import admin
+from .models import *
 
-#     search_fields = [
-#         'title',
-#         'caption',
-#         'author',
-#         'project_owner',
-#         'organization',
-#         'project',
-#         'product',
-#         'service',
-#         'post_location_city',
-#         'post_location_country',
-#         'post_location_continent',
-#         'post_location_planet',
-#         'business_knowledge',
-#     ]
-
-#     list_filter = (
-#         'status',
-#         'post_verification',
-#         'public_privacy',
-#         'post_type',
-#         'funding_infinite_end_date',
-#     )
-
-#     summernote_fields = (
-#         'business_knowledge',
-#         'caption',
-#         'introduction'
-#     )
-
-#     prepopulated_fields = {'slug': ('title',)}
-
-#     # ACTIONS
-#     actions = ['approved_post', 'frozen_post']
-
-#     def approved_post(self, request, queryset):
-#         queryset.update(status=1)
-
-#     def frozen_post(self, request, queryset):
-#         queryset.update(status=0)
+admin.site.register(Post)
