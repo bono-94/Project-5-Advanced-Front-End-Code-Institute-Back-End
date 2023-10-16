acdf02a8a2bd9400862ca22e605a98b4a17071cf
bono-94 <bono.94@hotmail.com>
2023-10-16 04:48:34 +0000

    Reverse non-pagination on containers page

--- containers/pagination.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.pagination import BasePagination
-
-class NoPagination(BasePagination):
-    def paginate_queryset(self, queryset, request, view=None):
-        return None
-
-    def get_paginated_response(self, data):
-        return data
